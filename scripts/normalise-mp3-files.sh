8fbcef575c5aee3dbfc1e17954d13bf1451300b6
Michael Robinson <mike@pagesofinterest.net>
2024-09-28 17:38:43 +1200

    Add mp3 normlisation script

--- scripts/normalise-mp3-files.sh
+++ scripts/normalise-mp3-files.sh
@@ -2,6 +2,7 @@
 
 # brew install aacgain
 # copy mp3 files out of ~/Library/Application Support/Anki2/<profile>/collection.media/*.mp3 to a working directory, consider backing them up as well.
+# find . -name '*.mp3'  -exec cp "{}" working-directory \;
 # cd to the working directory
 # execute this file
 # copy mp3 files back into the anki folder
